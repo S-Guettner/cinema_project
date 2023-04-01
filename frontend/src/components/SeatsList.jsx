fe8fb9e20c7b2f156978c081c4b5f1c678e724f0
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-01 17:56:57 +0200

    fix(SeatsList): updated props

--- frontend/src/components/SeatsList.jsx
+++ frontend/src/components/SeatsList.jsx
@@ -5,14 +5,33 @@ import { v4 as uuidv4 } from 'uuid'
 
 const SeatList = () => {
 
-    use
+    const [seatData,setSeatData] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:9090/api/v1/getSeats`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setSeatData(data)
+        })
+
+    },[])
 
     return ( 
         <main>
             <h1>SEATLIST</h1>
             <section>
                 <div className=" w-60">
-
+                    {seatData?.map((seat) => {
+                        return(
+                            <SingleSeat 
+                            key={uuidv4()}
+                            seatNumber={seat.seatNumber}
+                            seatStatus={seat.seatStatus}
+                            seatPrice={seat.seatPrice}
+                            />
+                        )
+                    })}
                 </div>
                 <div></div>
             </section>

752df6b9312261607935bcc4d19d62b02443d816
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-01 17:54:57 +0200

    feat(SingleSeat): added color var for tailwind class

--- frontend/src/components/SingleSeat.jsx
+++ frontend/src/components/SingleSeat.jsx
@@ -1,15 +1,19 @@
-import props from 'react'
+import { useState } from "react"
 
-const SingleSeat = (props) => {
 
-    console.log(props.status,props.id)
+const SingleSeat = ({seatNumber,seatStatus,seatPrice}) => {
+
+    //setting colors for seats
+    const color = seatNumber > 12 ? "green" : "yellow"
+    const seatColor = seatStatus === false ? color : "red"
 
     return ( 
-        <div className=" w-10 h-10 text-center rounded-full bg-slate-500 inline-block">
-            <p>{props.id}</p>
-            <p>{props.status}</p>
+        <div className={` w-10 h-10 text-center rounded-full bg-[${seatColor}] inline-block`}>
+            <p>{seatNumber}</p>
+            <p>{seatStatus.toString()}</p>
+            <p>{seatPrice}€</p>
         </div>
-     );
+     )
 }
  
 export default SingleSeat;

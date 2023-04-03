b2a9682864936a77bd02027fd815e2440165cb75
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-03 21:39:18 +0200

    fix: cors settings for deployment

--- server/index.js
+++ server/index.js
@@ -37,17 +37,6 @@ app.use(cors(
 
 // send .json data to DB
 
-/* only needed for sending data to DB */
-/* app.post('/api/v1/send' , async(req,res) => {
-    try {
-        const seatsData = await seatSchema.create(req.body)
-        res.status(200).json(seatsData)
-    } catch (err) {
-        console.log(err.message)
-        res.status(500).json({message: err.message})
-    }
-})
- */
 
 
 // get data from DB

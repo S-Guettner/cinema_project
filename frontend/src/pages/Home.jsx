2e6a3d00e355624674f172c4c8ac2f710b1423af
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-03 21:25:08 +0200

    styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return ( 
-        <main className="mx-auto w-60 border-2">
+        <main className="mx-auto w-60 ">
             <SeatList />
             <Link className="border-2 p-1 rounded-lg" to={'/dashboard'}>Link to Dashboard</Link>
         </main>

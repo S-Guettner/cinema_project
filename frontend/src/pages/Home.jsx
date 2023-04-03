eff3aa6427d91652127e27af53c6084e328fee20
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-03 21:24:37 +0200

    styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return ( 
         <main className="mx-auto w-60 border-2">
             <SeatList />
-            <Link className="border-2 p-2 rounded-lg" to={'/dashboard'}>Link to Dashboard</Link>
+            <Link className="border-2 p-1 rounded-lg" to={'/dashboard'}>Link to Dashboard</Link>
         </main>
      )
 }

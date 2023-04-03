533440d47bd4ebfb12b65b96ded0364858b5fbca
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-03 21:21:33 +0200

    fix: cors settings for deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return ( 
-        <main className="mx-auto w-60">
+        <main className="mx-auto w-60 border-2 p-2">
             <SeatList />
             <Link className="border-2 p-2 rounded-lg" to={'/dashboard'}>Link to Dashboard</Link>
         </main>
